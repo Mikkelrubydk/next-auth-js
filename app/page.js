5af8f5106c498c07bb5e44bcee4d809733f7daa7
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-11-18 16:57:57 +0100

    Update layout and page components for authentication example
    - Change metadata title and description for Auth.js example
    - Replace anchor tags with Next.js Link component for navigation
    - Add link to protected page in the Home component

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { auth } from "./auth";
 
 export default async function Home() {
@@ -12,11 +13,17 @@ export default async function Home() {
           This page is public. You can view this page without being signed in.
         </p>
 
-        <a
+        <Link
           className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
           href="/sign-in">
           Sign In
-        </a>
+        </Link>
+
+        <Link
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+          href="/protected">
+          Protected Page
+        </Link>
       </main>
     </div>
   );

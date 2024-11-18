1d7cfd4d2dc5d608b79462f68584b06370be2d09
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-11-18 17:23:09 +0100

    Refactor sign-in form handling to use a dedicated async function

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -1,19 +1,21 @@
 import { signIn } from "../auth";
 
 export default function SignIn() {
+  async function handleSignIn(formData) {
+    "use server";
+    signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirectTo: "/protected"
+    });
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-5xl text-">Sign in</h1>
         <form
-          action={async formData => {
-            "use server";
-            await signIn("credentials", {
-              email: formData.get("email"),
-              password: formData.get("password"),
-              redirectTo: "/protected"
-            });
-          }}
+          action={handleSignIn}
           className="flex flex-col gap-4 w-full max-w-[30ch]">
           <label className="flex flex-col gap-1">
             <span>Email</span>
